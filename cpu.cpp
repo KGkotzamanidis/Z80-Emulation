f292d12e37a2664f90c151f3c813b4b7aa513ea8
kgkotzamanidis <konstantinosgkotzamanidis@outlook.com>
2021-01-17 16:25:02 +0200

    cpu
    Create the main body for opcode and address functions

--- cpu.cpp
+++ cpu.cpp
@@ -1,8 +1,73 @@
 #include "cpu.h"
 #include "bus.h"
+CPU::CPU(){
+    instructionTable = {
+    };
+}
+
+/*
+ *  Λειτουργια διευθυνσεων
+ */
+//  Immediate_Addressing
+uint8_t CPU::Immediate_Addressing(){
+    return 0x00;
+}
+//  Immediate_Extended_Addressing
+uint8_t CPU::Immediate_Extended_Addressing(){
+    return 0x00;
+}
+//  Modified_Page_Zero_Addressing
+uint8_t CPU::Modified_Page_Zero_Addressing(){
+    return 0x00;
+}
+//  Relative_Addressing
+uint8_t CPU::Relative_Addressing(){
+    return 0x00;
+}
+//  Extended_Addressing
+uint8_t CPU::Extended_Addressing(){
+    return 0x00;
+}
+//  Indexed_Addressing
+uint8_t CPU::Indexed_Addressing(){
+    return 0x00;
+}
+//  Register_Addressing
+uint8_t CPU::Register_Addressing(){
+    return 0x00;
+}
+//  Implied_Addressing
+uint8_t CPU::Implied_Addressing(){
+    return 0x00;
+}
+//  Register_Indirect_Addressing
+uint8_t CPU::Register_Indirect_Addressing(){
+    return 0x00;
+}
+
+/*
+ *  Εντολες CPU
+ */
+
+//  NOP Function
+uint8_t CPU::NOP(){
+    switch (opcodes) {
+        case 0x00:
+            return 1;
+        break;
+    }
+    return 0;
+}
 
 void CPU::connctCPU(BUS *n){
     bus =n;
+}
+
+void CPU::printSpect(){
+    using namespace std;
+    cout<<"~System Information~"<<endl;
+    cout<<"CPUname:Z80"<<endl;
+    cout<<"CPUarchi:8bit"<<endl;
 }
 
 uint8_t CPU::readCPU(uint16_t address){
@@ -24,3 +89,15 @@ void CPU::setFLAG(regF anFlag,bool avalue){
         regStatus &= ~anFlag;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- cpu.h
+++ cpu.h
@@ -1,11 +1,12 @@
 #ifndef _CPU_H
 #define _CPU_H
 
+#include <iostream>
 #include <cstdint>
 #include <string>
+#include <map>
 #include <vector>
 
-typedef uint8_t bit;
 class BUS;
 class CPU{
     public:
@@ -37,7 +38,9 @@ class CPU{
         s=(1<<7)   //7 Bit Sign Flag.
     };
 
+    CPU();
     void connctCPU(BUS *n);
+    void printSpect();
     private:
     uint8_t fetched=0x00,opcodes=0x00,cycles=0;
     uint16_t address_abs=0x0000,address_rel=0x0000;
@@ -48,19 +51,20 @@ class CPU{
      * θα χρειαστει.
      */
     struct Instruction_CPU{
-        std::string name;
+        std::string instruction_name;
         uint8_t (CPU::*operate)     (void) = nullptr;
         uint8_t (CPU::*addressmode) (void) = nullptr;
         uint8_t cycles=0;
     };
     /*
-     * Για να δεις το table πανε εδω:
-     * https://pastraiser.com/cpu/i8080/i8080_opcodes.html
+     * vector<Instruction_CPU> instructionTable:
+     * Για να δεις το table πανε εδω: https://clrhome.org/table/
      */
-    std::vector<Instruction_CPU> _lookupTable;
+    std::vector<Instruction_CPU> instructionTable;
     uint8_t fetch();
     /*
      * Λειτουργια διευθυνσεων
+     * 9 Εντολες διευθυνσεων(addressing)
      * Βλεπε σελιδα 34.
      */
     uint8_t Immediate_Addressing();             //1
@@ -72,16 +76,28 @@ class CPU{
     uint8_t Register_Addressing();              //7
     uint8_t Implied_Addressing();               //8
     uint8_t Register_Indirect_Addressing();     //9
-
     /*
      * Εντολες CPU
+     * 68 Βασικες εντολες(opcodes)
+     * Βλεπε σελιδα 70.
      */
-    uint8_t LD();uint8_t LDA();uint8_t LDI();
-
-
-
-
-
+    uint8_t ADC();  uint8_t ADD();  uint8_t AND();  uint8_t BIT();
+    uint8_t CALL(); uint8_t CCF();  uint8_t CP();   uint8_t CPD();
+    uint8_t CPDR(); uint8_t CPI();  uint8_t CPIR(); uint8_t CPL();
+    uint8_t DAA();  uint8_t DEC();  uint8_t DI();   uint8_t DJNZ();
+    uint8_t EI();   uint8_t EX();   uint8_t EXX();  uint8_t HALT();
+    uint8_t IM();   uint8_t IN();   uint8_t INC();  uint8_t IND();
+    uint8_t INDR(); uint8_t INI();  uint8_t INIR(); uint8_t JP();
+    uint8_t JR();   uint8_t LD();   uint8_t LDD();  uint8_t LDDR();
+    uint8_t LDI();  uint8_t LDIR(); uint8_t NEG();  uint8_t NOP();
+    uint8_t OR();   uint8_t OTDR(); uint8_t OTIR(); uint8_t OUT();
+    uint8_t OUTD(); uint8_t OUTI(); uint8_t POP();  uint8_t PUSH();
+    uint8_t RES();  uint8_t RET();  uint8_t RETI(); uint8_t RETN();
+    uint8_t RL();   uint8_t RLA();  uint8_t RLC();  uint8_t RLCA();
+    uint8_t RLD();  uint8_t RR();   uint8_t RRA();  uint8_t RRC();
+    uint8_t RRCA(); uint8_t RRD();  uint8_t RST();  uint8_t SBC();
+    uint8_t SCF();  uint8_t SET();  uint8_t SLA();  uint8_t SLL_SL1();
+    uint8_t SRA();  uint8_t SRL();  uint8_t SUB();  uint8_t XOR();
 
 
     BUS *bus = nullptr;
